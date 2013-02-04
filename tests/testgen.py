775079271971e027fb553d9e77c00e2cfc1455da
Vivek Thampi <vivek.mt@gmail.com>
2013-02-03 22:01:40 -0800

    Add new test generator
    This change replaces the testgen_opr.py based test generator with
    a more inline operand generator. This allows for more flexible decision
    making and reuse a lot of operand values.

--- tests/testgen.py
+++ tests/testgen.py
@@ -27,7 +27,7 @@ import os
 import sys
 import random
 
-if ( len( os.getenv( 'UD_SCRIPT_DIR' ) ) ):
+if ( len( os.getenv( 'UD_SCRIPT_DIR', "" ) ) ):
     scriptsPath = os.getenv( 'UD_SCRIPT_DIR' ) + "/scripts"
 else:
     scriptsPath = '../scripts'
@@ -37,17 +37,127 @@ import ud_optable
 import ud_opcode
 import testgen_opr
 
+def bits2name(bits):
+    bits2name_map = {
+         8 : "byte",
+        16 : "word",
+        32 : "dword",
+        64 : "qword",
+    }
+    return bits2name_map[bits]
+
+
 class UdTestGenerator( ud_opcode.UdOpcodeTables ):
 
     OprTable = []
 
     ExcludeList = ( 'fcomp3', 'fcom2', 'fcomp5', 'fstp1', 'fstp8', 'fstp9',
-                    'fxch4', 'fxch7' )
+                    'fxch4', 'fxch7', 'xchg', 'pop' )
 
-    def __init__( self ):
+    def __init__(self, mode):
+        self.mode = mode
         pass
 
+    def OprMem(self, size, cast=False):
+        choices = []
+        if self.mode < 64:
+            choices = ["[bx+si+0x1234]",
+                       "[bx+0x10]", 
+                       "[bp+di+0x27]",
+                       "[di+0x100]"]
+        choices.extend(("[eax+ebx]", "[ebx+ecx*4]", 
+                        "[ebp+0x10]"))
+        if self.mode == 64:
+            choices.extend(("[rax+rbx]", "[rbx+r8-0x10]"))
+        addr = random.choice(choices)
+        if cast:
+            addr = "%s %s" % (bits2name(size), addr)
+        return addr
+
+    def Gpr(self, size):
+        if size == 8:
+            choices = ['al', 'cl']
+            if self.mode == 64:
+                choices.extend(['sil', 'r10b'])
+        elif size == 16:
+            choices = ['ax', 'bp', 'dx']
+            if self.mode == 64:
+                choices.extend(['r8w', 'r14w'])
+        elif size == 32:
+            choices = ['eax', 'ebp', 'edx']
+            if self.mode == 64:
+                choices.extend(['r10d', 'r12d'])
+        elif size == 64:
+            choices = ['rax', 'rsi', 'rsp']
+            if self.mode == 64:
+                choices.extend(['r9', 'r13'])
+        return random.choice(choices)
+
+    def Xmm(self):
+        r = 16 if self.mode == 64 else 8
+        return "xmm%d" % random.choice(range(r))
+
+    def Mmx(self):
+        return "mm%d" % random.choice(range(8))
+
+    def Modrm_RM_GPR(self, size, cast=False):
+        return random.choice([self.Gpr(size),
+                              self.OprMem(size=size, cast=cast)])
+
+    def Modrm_RM_XMM(self, size, cast=False):
+        return random.choice([self.Xmm(),
+                              self.OprMem(size=size, cast=cast)])
+
+    def Opr_Gb(self):
+        return self.Gpr(8)
+
+    def Opr_Eb(self):
+        return self.Modrm_RM_GPR(8)
+
+    def Opr_Ew(self):
+        return self.Modrm_RM_GPR(16)
+
+    def Insn_Ev(self):
+        choices = [self.Modrm_RM_GPR(16, cast=True),
+                   self.Modrm_RM_GPR(32, cast=True)]
+        if self.mode == 64:
+            choices.append(self.Modrm_RM_GPR(64, cast=True))
+        return [random.choice(choices)]
+
+    def Opr_V(self):
+        return self.Xmm()
+
+    def Opr_W(self):
+        return random.choice([self.Xmm(), self.OprMem(size=128)])
+
+    def Opr_P(self):
+        return self.Mmx()
+
+    def Opr_Q(self, cast=False):
+        return random.choice([self.Mmx(), self.OprMem(size=64, cast=cast)])
+
+    def Insn_Eb(self):
+        return [self.Modrm_RM_GPR(size=8, cast=True)]
+
+    def Insn_Ew(self):
+        return [self.Modrm_RM_GPR(size=16, cast=True)]
+
+    def Insn_Ev_Gv(self):
+        choices = [(self.Modrm_RM_GPR(16), self.Gpr(16)),
+                   (self.Modrm_RM_GPR(32), self.Gpr(32))]
+        if self.mode == 64:
+            choices.append((self.Modrm_RM_GPR(64), self.Gpr(64)))
+        return random.choice(choices)
+
+    def Insn_Gv_Ev(self):
+        x, y = self.Insn_Ev_Gv();
+        return (y, x)
+
+    def Insn_V_Q(self):
+        return [self.Opr_V(), self.Opr_Q(cast=True)]
+
     def generate_yasm( self, mode, seed ):
+        opr_combos = {}
         random.seed( seed )
         print "[bits %s]" % mode
         for insn in self.InsnTable:
@@ -57,21 +167,44 @@ class UdTestGenerator( ud_opcode.UdOpcodeTables ):
                 continue
             if 'def64' in insn[ 'prefixes' ] and mode != '64':
                 continue
-            opr = '_'.join( insn[ 'operands' ] )
-            if len( opr ):
-                if not opr in testgen_opr.OperandSet.keys():
-                    pass
-                    # print "Warning: no test-case for operand type '%s' (insn=%s)" % ( opr, insn[ 'mnemonic' ] )
-                try:
-                    testcase = testgen_opr.OperandSet[ opr ][ mode ]
-                    if len( testcase ):
-                        print "\t%s %s" % ( insn[ 'mnemonic' ], random.choice( testcase ) )
-                except KeyError:
-                    # ignore key errors (which are really coverage errors)
-                    pass
+
+            if len(insn['operands']) == 0:
+                continue
+                # print "\t%s" % insn['mnemonic']
+
+            fusedName = '_'.join(insn['operands'])
+            if fusedName not in opr_combos:
+                opr_combos[fusedName] = { 'covered' : False, 'freq' : 0 }
+            opr_combos[fusedName]['freq'] += 1
+
+            fn = getattr(self, "Insn_" + fusedName , None)
+            if fn is not None:
+                operands = ", ".join(fn())
+            else: 
+                oprgens = [ getattr(self, "Opr_" + opr, None) 
+                                for opr in insn['operands'] ]
+                if None not in oprgens:
+                    operands = ", ".join([ oprgen() for oprgen in oprgens ])
+                else:
+                    operands = None
+            if operands is not None:
+                print "\t%s %s" % (insn['mnemonic'], operands)
+                opr_combos[fusedName]['covered'] = True
+
+        # stats
+        total = 0
+        covered = 0
+        for combo in sorted(opr_combos, key=lambda k: opr_combos[k]['freq']):
+            total += 1
+            covered += (1 if opr_combos[combo]['covered'] else 0)
+            sys.stderr.write("==> %12s : %5d %s\n" % 
+                                (combo, opr_combos[combo]['freq'], 
+                                 "!covered" if not opr_combos[combo]['covered']
+                                                else ''))
+        sys.stderr.write("==> Coverage = %d / %d (%d%%)\n" % (covered, total, (100 * covered / total)))
 
 def main():
-    generator = UdTestGenerator()
+    generator = UdTestGenerator(int(sys.argv[3]))
     optableXmlParser = ud_optable.UdOptableXmlParser()
     optableXmlParser.parse( sys.argv[ 1 ], generator.addInsnDef )
 
