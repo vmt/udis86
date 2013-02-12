148824bd078a4f69c20e87418bc357871a325d6d
spazzarama <spazzarama@gmail.com>
2013-02-12 17:47:28 +1100

    Implemented Visual Studio 2010 build support

--- libudis86/types.h
+++ libudis86/types.h
@@ -47,14 +47,8 @@
 
 #ifdef _MSC_VER
 # define FMT64 "%I64"
-  typedef unsigned __int8 uint8_t;
-  typedef unsigned __int16 uint16_t;
-  typedef unsigned __int32 uint32_t;
-  typedef unsigned __int64 uint64_t;
-  typedef __int8 int8_t;
-  typedef __int16 int16_t;
-  typedef __int32 int32_t;
-  typedef __int64 int64_t;
+#include <stdint.h>
+#define inline __inline /* MS Visual Studio requires __inline instead of inline for C code */
 #else
 # define FMT64 "%ll"
 # ifndef __UD_STANDALONE__
