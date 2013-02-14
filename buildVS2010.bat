e7c5f2d654e7cfb2912255c302376ad031a2a5dc
spazzarama <spazzarama@gmail.com>
2013-02-14 20:26:41 +1100

    Moved VS2010 build to a separate directory

--- buildVS2010.bat
+++
@@ -1,7 +0,0 @@
-echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86_amd64
-
-msbuild.exe udis86.sln /t:Clean
-msbuild.exe build.proj /t:Clean,BuildRelease_x86,BuildRelease_x64,PostBuild
-REM msbuild.exe build.proj /t:Clean,BuildRelease_x86,BuildRelease_x64,BuildDebug_x86,BuildDebug_x64,PostBuild
-pause
