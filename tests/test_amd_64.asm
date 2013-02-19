e28ce2a7961ce5261ae2a21d222777c2528afad7
Vivek Thampi <vivek.mt@gmail.com>
2013-02-18 18:56:43 -0800

    Delete obsolete files

--- tests/test_amd_64.asm
+++
@@ -1,7 +0,0 @@
-;; Test amd specific 64bit instructions
-
-[bits 64]
-
-    ;; Invalid instructions in amd 64bit mode
-    db 0x0f, 0x34    ; sysenter (invalid)
-    db 0x0f, 0x35    ; sysexit  (invalid)
