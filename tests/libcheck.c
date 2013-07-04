c635bdfcaead081bc22c2ed44e211010a117137f
Vivek Thampi <vivek.mt@gmail.com>
2013-07-04 00:22:25 -0700

    Add some more api unit tests

--- tests/libcheck.c
+++ tests/libcheck.c
@@ -43,9 +43,62 @@ unsigned int testcase_check_fails;
   } while (0)
 
 #define TEST_CHECK_OP_REG(o, n, r) \
-  TEST_CHECK((o)->operand[n].type == UD_OP_REG && \
-             (o)->operand[n].base == (r))
+  TEST_CHECK(ud_insn_opr(o, n)->type == UD_OP_REG && \
+             ud_insn_opr(o, n)->base == (r))
 
+
+static int
+input_callback(ud_t *u)
+{
+  int *n = (int *) ud_get_user_opaque_data(u);
+  if (*n == 0) {
+    return UD_EOI;
+  }
+  --*n;
+  return 0x90; 
+}
+
+static void
+check_input(ud_t *ud_obj)
+{
+  TEST_DECL("check_input");
+  const uint8_t code[] = { 0x89, 0xc8 }; /* mov eax, ecx */
+  int i;
+
+  /* truncate buffer */
+  ud_set_mode(ud_obj, 32);
+  for (i = 0; i < 5; ++i) {
+    ud_set_input_buffer(ud_obj, code, (sizeof code) - 1); 
+    TEST_CHECK(ud_disassemble(ud_obj) == 1);
+    TEST_CHECK(ud_insn_len(ud_obj) == 1);
+    TEST_CHECK(ud_obj->mnemonic == UD_Iinvalid);
+  }
+
+  /* input hook test */
+  {
+    int n;
+    ud_set_user_opaque_data(ud_obj, (void *) &n);
+    ud_set_input_hook(ud_obj, &input_callback);
+
+    n = 0;
+    TEST_CHECK(ud_disassemble(ud_obj) == 0);
+
+    n = 1;
+    ud_set_input_hook(ud_obj, &input_callback);
+    TEST_CHECK(ud_disassemble(ud_obj) == 1);
+    TEST_CHECK(ud_obj->mnemonic == UD_Inop);
+
+    n = 2;
+    ud_set_input_hook(ud_obj, &input_callback);
+    ud_input_skip(ud_obj, 1);
+    TEST_CHECK(ud_disassemble(ud_obj) == 1);
+    TEST_CHECK(ud_obj->mnemonic == UD_Inop);
+    TEST_CHECK(ud_disassemble(ud_obj) == 0);
+    TEST_CHECK(ud_insn_len(ud_obj) == 0);
+    TEST_CHECK(ud_obj->mnemonic == UD_Iinvalid);
+  }
+}
+  
 static void
 check_mode(ud_t *ud_obj)
 {
@@ -58,6 +111,25 @@ check_mode(ud_t *ud_obj)
   TEST_CHECK_OP_REG(ud_obj, 1, UD_R_ECX);
 }
 
+static void
+check_disasm(ud_t *ud_obj)
+{
+  TEST_DECL("check_mode");
+  const uint8_t code[] = { 0x89, 0xc8,  /* mov eax, ecx */
+                           0x90 };      /* nop */
+  ud_set_input_buffer(ud_obj, code, sizeof code); 
+  ud_set_mode(ud_obj, 32);
+  ud_set_pc(ud_obj, 0x100);
+
+  TEST_CHECK(ud_disassemble(ud_obj) == 2);
+  TEST_CHECK(ud_insn_off(ud_obj) == 0x100);
+  TEST_CHECK(ud_insn_ptr(ud_obj)[0] == 0x89);
+  TEST_CHECK(ud_insn_ptr(ud_obj)[1] == 0xc8);
+
+  TEST_CHECK(ud_disassemble(ud_obj) == 1);
+  TEST_CHECK(ud_insn_off(ud_obj) == 0x102);
+  TEST_CHECK(ud_insn_ptr(ud_obj)[0] == 0x90);
+}
 
 int
 main(void)
@@ -66,7 +138,9 @@ main(void)
   ud_init(&ud_obj);
   ud_set_syntax(&ud_obj, UD_SYN_INTEL);
 
+  check_input(&ud_obj);
   check_mode(&ud_obj);
+  check_disasm(&ud_obj);
 
   if (testcase_check_fails > 0) {
     printf("libcheck result: %d checks, %d failures\n",
