c6bb49a1eef24fa30cd53db4aab259f73f485ba3
Vivek Thampi <vivek.mt@gmail.com>
2013-07-06 12:44:17 -0700

    New avx instructions starting with "c"

--- tests/asm/32/avx.asm
+++ tests/asm/32/avx.asm
@@ -4,3 +4,15 @@
     vaddps ymm1, ymm2, ymm3
     vaddps ymm1, ymm7, [eax]
     vblendpd ymm1, ymm7, ymm4, 0x42 
+    vcvtpd2ps xmm1, xmm2 
+    vcvtpd2ps xmm1, ymm3
+    vcvtpd2ps xmm1, oword [eax]
+    vcvtpd2ps xmm1, yword [eax]
+    vcvtpd2dq xmm1, xmm2 
+    vcvtpd2dq xmm1, ymm3
+    vcvtpd2dq xmm1, oword [eax]
+    vcvtpd2dq xmm1, yword [eax]
+    vcvttpd2dq xmm1, xmm2 
+    vcvttpd2dq xmm1, ymm3
+    vcvttpd2dq xmm1, oword [eax]
+    vcvttpd2dq xmm1, yword [eax]
